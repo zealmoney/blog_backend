067337bbc0217f5d6ccfa7d5d038dfba0b0bba3a
zealmoney <chatwithzeal@gmail.com>
2023-09-06 14:02:09 -0500

    first commit

--- backend/urls.py
+++ backend/urls.py
@@ -37,3 +37,4 @@ urlpatterns = [
     path('api/', include(router.urls))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
